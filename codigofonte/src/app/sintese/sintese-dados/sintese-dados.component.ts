57a63d28f745d0ea8ac713419ddbb0f36da3396a
Elias Correa Silva <elias.silva@ibge.gov.br>
2017-01-18 15:25:40 -0200

    criação do componente de dados da síntese

---
+++ codigofonte/src/app/sintese/sintese-dados/sintese-dados.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'sintese-dados',
+    templateUrl: 'sintese-dados.template.html',
+    styleUrls: ['sintese-dados.style.css']
+})
+export class SinteseDadosComponent {
+
+    
+
+}
--- codigofonte/src/app/sintese/sintese.module.ts
+++ codigofonte/src/app/sintese/sintese.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 
 import { SinteseComponent } from './sintese.component';
 import { SinteseHeaderComponent } from './sintese-header/sintese-header.component';
+import { SinteseDadosComponent } from './sintese-dados/sintese-dados.component';
 
 @NgModule({
     declarations: [
         SinteseComponent,
-        SinteseHeaderComponent
+        SinteseHeaderComponent,
+        SinteseDadosComponent
     ]
 })
 export class SinteseModule {}
