e5ee785ff00b7c8445a74a6427d95e109838726c
Elias Correa Silva <elias.silva@ibge.gov.br>
2017-01-23 09:38:13 -0200

    inicio da integração com o serviço da síntese

--- codigofonte/src/app/sintese/sintese-header/sintese-header.component.ts
+++ codigofonte/src/app/sintese/sintese-header/sintese-header.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, OnChanges } from '@angular/core';
 import {RouterParamsService} from '../../shared/router-params.service'
+import {SinteseService} from '../sintese.service'
 
 @Component({
     selector: 'sintese-header',
@@ -13,15 +14,19 @@ export class SinteseHeaderComponent {
     pesquisa;
 
     constructor(
-        private _routerParams:RouterParamsService
+        private _routerParams:RouterParamsService,
+        private _sintese:SinteseService
     ){}
 
     ngOnInit(){
         this._routerParams.params$.subscribe((params)=>{
             console.log(params);
+            this._sintese.getDetalhesIndicadorSintese('330455', '29171').subscribe((dados) => {
+                console.log(dados);
+            });
             this.titulo = params.indicador; //pegar o nome real do indicador e da pesquisa de onde ele vem
             this.pesquisa = 'Censo';
-        })
+        });
     }
 
     ativar(tipo){
