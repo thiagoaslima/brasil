790c1346c254a994fe545b1b13ba526b8ac85ac0
IBGE\rafael.rocha <rafael.rocha@ibge.gov.br>
2017-01-27 15:20:58 -0200

    modulo pesquisa-dados

---
+++ codigofonte/src/app/pesquisa/pesquisa-dados/pesquisa-dados.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit, OnChanges } from '@angular/core';
+
+@Component({
+    selector: 'pesquisa-dados',
+    templateUrl: 'pesquisa-dados.template.html',
+    styleUrls: ['pesquisa-dados.style.css']
+})
+
+export class PesquisaDadosComponent {
+
+}
