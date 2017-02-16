c02b39d676b13577c0dd8e7c647cb96ba551a54b
IBGE\jose.serra <jose.serra@ibge.gov.br>
2017-02-16 14:27:51 -0200

    fontes e notas nas pesquisas

--- codigofonte/src/app/sintese/sintese-detalhes/sintese-detalhes.component.ts
+++ codigofonte/src/app/sintese/sintese-detalhes/sintese-detalhes.component.ts
@@ -134,7 +134,7 @@ export class SinteseDetalhesComponent implements OnInit {
                 return infoPesquisa;
             }).subscribe(info => {
                 console.log(info);
-                debugger;
+                //debugger;
                 this.fontesIndicador = !!info.periodos ? info.periodos : [];
                 info.periodos.forEach(periodo => {
                     this.temFonte = periodo.fonte.length > 0 ? true : false;
