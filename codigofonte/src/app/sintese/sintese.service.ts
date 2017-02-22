f5c8a0882f9f9a99818d4b1e34c48907f0ebdd66
Leandro de Araujo Loures Coelho <leandro.coelho@ibge.gov.br>
2017-02-22 11:32:01 -0300

    Ajuste header.

--- codigofonte/src/app/sintese/sintese.service.ts
+++ codigofonte/src/app/sintese/sintese.service.ts
@@ -15,7 +15,7 @@ import { ufs } from '../../api/ufs';
 export class SinteseService{
 
     // Lista de pesquisas estão autorizadas a serem acessadas pelo serviço.
-    private idPesquisasValidas: number[] = [11, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 29, 30, 31, 32, 34, 35, 36, 37, 38, 39, 40, 42, 43];
+    private idPesquisasValidas: number[] = [11, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 42, 43];
 
     constructor(private _http: Http) {  }
 
