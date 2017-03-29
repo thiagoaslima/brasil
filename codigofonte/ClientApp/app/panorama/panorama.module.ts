3dbeca63a804d92e925e375d73aaa6419fe958c1
Rafael Rocha <rafael.rocha@ibge.gov.br>
2017-03-29 14:19:56 -0300

    panorama card

--- codigofonte/ClientApp/app/panorama/panorama.module.ts
+++ codigofonte/ClientApp/app/panorama/panorama.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 import { PanoramaComponent } from './panorama.component';
 import { PamoramaResumoComponent } from './panorama-resumo/panorama-resumo.component'
 import { PanoramaTemasComponent } from './panorama-temas/panorama-temas.component';
+import { PanoramaCardComponent } from './panorama-temas/panorama-card/panorama-card.component';
 
 @NgModule({
     imports: [],
@@ -10,7 +11,8 @@ import { PanoramaTemasComponent } from './panorama-temas/panorama-temas.componen
     declarations: [
         PanoramaComponent,
         PamoramaResumoComponent,
-        PanoramaTemasComponent
+        PanoramaTemasComponent,
+        PanoramaCardComponent
     ],
     providers: [],
 })
