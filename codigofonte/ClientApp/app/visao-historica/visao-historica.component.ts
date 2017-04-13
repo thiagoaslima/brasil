6a8594c1baf0fee5588fd582dc1b7ed5268ff686
Leandro de Araujo Loures Coelho <leandro.coelho@ibge.gov.br>
2017-04-13 15:18:37 -0300

    Inclusão historico e fotos.

---
+++ codigofonte/ClientApp/app/visao-historica/visao-historica.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'visao-historica',
+    templateUrl: 'visao-historica.template.html'
+})
+
+export class VisaoHistoricaComponent implements OnInit {
+    constructor() { }
+
+    ngOnInit() { }
+}
