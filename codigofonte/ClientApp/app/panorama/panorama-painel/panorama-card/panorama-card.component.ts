294ca8ac8aee4aa112c21eb0e074987aa293f8a0
Thiago Lima <thiagoaslima@gmail.com>
2017-04-20 13:36:32 -0300

    retorno de varias requisições até o serviço de ranking ter melhor performance

--- codigofonte/ClientApp/app/panorama/panorama-painel/panorama-card/panorama-card.component.ts
+++ codigofonte/ClientApp/app/panorama/panorama-painel/panorama-card/panorama-card.component.ts
@@ -39,7 +39,7 @@ export class PanoramaCardComponent implements OnInit, OnChanges {
     ) { }
 
     ngOnInit() { 
-       /*
+       
         const sync$ = this._dados$
             .do(dados => {debugger; console.log(dados)})
             .distinct( (a, b) =>  ['pesquisaId', 'indicadorId', 'periodo'].every(key => a[key] === b[key]))
@@ -67,7 +67,7 @@ export class PanoramaCardComponent implements OnInit, OnChanges {
         this.rankingUf$ = sync$
             .flatMap(obj => this._indicadorService.getPosicaoRelativa(obj.pesquisaId, obj.indicadorId, obj.periodo, obj.localidade.codigo, obj.localidade.parent.codigo))
             .share()
-            */
+            
     }
 
     ngOnChanges(changes: SimpleChanges) {
@@ -109,15 +109,15 @@ export class PanoramaCardComponent implements OnInit, OnChanges {
     template:`
         <div class="card__regua-comparacao">
             <p>{{title}}</p>
-            <div class="card__regua-comparacao__regua" [class.card__regua-comparacao__regua--desabilitada]="!ranking" [attr.pos]="posicao" [attr.len]="itens" [attr.ranking]="ranking">
-                <div [ngStyle]='{right: cssRanking}' [title]="this.posicao + ' de ' + this.itens " class="card__regua-comparacao__regua__marcador"></div>
+            <div class="card__regua-comparacao__regua" [class.card__regua-comparacao__regua--desabilitada]="!ranking" [attr.pos]="this.rankingObj?.ranking" [attr.len]="itens" [attr.ranking]="ranking">
+                <div [ngStyle]='{right: cssRanking}' [title]="this.rankingObj?.ranking + ' de ' + this.itens " class="card__regua-comparacao__regua__marcador"></div>
             </div>
         </div>
     `,
     changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class PanoramaCardReguaComponent implements OnChanges {
-    @Input() posicao;
+    @Input('ranking') rankingObj;
     @Input() itens;
     @Input() title;
 
@@ -125,9 +125,10 @@ export class PanoramaCardReguaComponent implements OnChanges {
     public cssRanking;
 
     ngOnChanges(changes: SimpleChanges) {
-        if (this.posicao && this.itens) {
-            this.ranking = `${(this.posicao/this.itens * 100).toFixed(2)}%`;
-            this.cssRanking = `${((this.posicao/this.itens * 100) * 96 / 100 ).toFixed(2)}%`;
+        if (this.rankingObj && this.itens) {
+            debugger;
+            this.ranking = `${(this.rankingObj.ranking/this.rankingObj.qtdeItensComparados * 100).toFixed(2)}%`;
+            this.cssRanking = `${((this.rankingObj.ranking/this.rankingObj.qtdeItensComparados * 100) * 96 / 100 ).toFixed(2)}%`;
         } else {
             this.ranking = null;
             this.cssRanking = null;
