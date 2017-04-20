2c404c17c2da34c595ee9132310ee0f7b06ce334
Thiago Lima <thiagoaslima@gmail.com>
2017-04-20 18:55:23 -0300

    analytics e barra gov federal

--- codigofonte/ClientApp/app/shared/router-params.service.ts
+++ codigofonte/ClientApp/app/shared/router-params.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Router, ActivatedRoute, ActivatedRouteSnapshot, NavigationEnd } from '@angular/router';
+import { isBrowser } from 'angular2-universal'
 
 import { Observable } from 'rxjs';
 import 'rxjs/add/operator/distinctUntilChanged';
@@ -20,7 +21,18 @@ export class RouterParamsService {
             .filter(e => e instanceof NavigationEnd)
             .distinctUntilChanged()
             .map(e => this.extractParamsFromTree(this._route.snapshot, {}))
-            //.share();
+        //.share();
+
+        this._router.events
+            .filter(e => e instanceof NavigationEnd)
+            .distinctUntilChanged()
+            .subscribe(e => {
+                debugger;
+                if (isBrowser && (<any>window).ga) {
+                    (<any>window).ga('set', 'page', e.url);
+                    (<any>window).ga('send', 'pageview');
+                }
+            })
     }
 
 
@@ -28,15 +40,15 @@ export class RouterParamsService {
         return route.children.reduce((agg, route) => route.outlet === outlet ? route : agg, null);
     }
 
-    private extractParamsFromTree(route, {params = {}, queryParams = {}}, outlet = 'primary') {
+    private extractParamsFromTree(route, { params = {}, queryParams = {} }, outlet = 'primary') {
         let _params = {
             params,
             queryParams
         };
 
         if (!route) return _params;
-        
-        Object.assign(_params.params, Object.keys(route.params).reduce( (agg, key) => {
+
+        Object.assign(_params.params, Object.keys(route.params).reduce((agg, key) => {
             let val = decodeURIComponent(route.params[key]);
             agg[key] = val;
             return agg;
