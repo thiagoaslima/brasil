1a81bc9d4235251464353c734b2d6a08d857b9e5
Elias Correa Silva <elias.silva@ibge.gov.br>
2017-05-17 17:44:53 -0300

    implemetação do componente da linha do tempo

---
+++ codigofonte/ClientApp/app/infografia/linha-tempo/linha-tempo.component.ts
@@ -0,0 +1,29 @@
+import { Component, EventEmitter, Input, Output, OnInit, OnChanges } from '@angular/core';
+
+@Component({
+    selector: 'linha-tempo',
+    templateUrl: 'linha-tempo.template.html',
+    styleUrls: ['linha-tempo.style.css']
+})
+export class LinhaTempo implements OnInit, OnChanges {
+
+    @Input() anos : Number[];
+    @Input() indexSelecionado = 0;
+
+    @Output() onAno = new EventEmitter();
+
+    ngOnChanges(){
+
+    }
+
+    ngOnInit(){
+
+    }
+
+    onSelect(index){
+        this.indexSelecionado = index;
+        this.onAno.emit(this.anos[index]);
+    }
+
+}
+
