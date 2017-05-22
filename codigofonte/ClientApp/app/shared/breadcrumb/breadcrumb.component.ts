fa3a2207b16cb6cecb850e5eb477cf535183d038
Fernando Mendonça de Almeida <fernando.almeida@ibge.gov.br>
2017-05-22 15:13:16 -0300

    Adaptação do pesquisa pai para passar informações ao breadcrumb

--- codigofonte/ClientApp/app/shared/breadcrumb/breadcrumb.component.ts
+++ codigofonte/ClientApp/app/shared/breadcrumb/breadcrumb.component.ts
@@ -8,7 +8,7 @@ import { Component, EventEmitter, Input, Output, OnInit, OnChanges } from '@angu
 export class Breadcrumb implements OnInit, OnChanges {
 
     @Input() indicadores; //array linear de indicadores (já com flat aplicado)
-    @Input() indicadorSelecionado; // id do indicador selecionado
+    @Input() indicadorSelecionado; // modelo indicador selecionado
     @Input() urlBase = '';
 
     caminho = [];
@@ -17,9 +17,9 @@ export class Breadcrumb implements OnInit, OnChanges {
         this.caminho = [];
         if(this.indicadores){
             for(let i = 0; i < this.indicadores.length; i++){
-                if(this.indicadores[i].id == this.indicadorSelecionado){
+                if(this.indicadores[i].id == this.indicadorSelecionado.id){
                     let nivel = this.indicadores[i].nivel;
-                    this.caminho = [{'nome' : this.indicadores[i].indicador, 'id' : this.indicadores[i].id}];
+                    this.caminho = [{'nome' : this.indicadores[i].nome, 'id' : this.indicadores[i].id}];
                     for(; i >= 0; i--){
                         if(this.indicadores[i].nivel == nivel - 1){
                             nivel -= 1;
