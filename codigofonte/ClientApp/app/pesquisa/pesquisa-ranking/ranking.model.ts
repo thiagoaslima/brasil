09015e11a5f9e25e8b3ca32d37f5d42ac65425fe
Leandro de Araujo Loures Coelho <leandro.coelho@ibge.gov.br>
2017-05-26 17:59:21 -0300

    Implementação do Rabking.

---
+++ codigofonte/ClientApp/app/pesquisa/pesquisa-ranking/ranking.model.ts
@@ -0,0 +1,41 @@
+import { Localidade } from '../../shared2/localidade/localidade.model';
+
+
+
+export class RankingLocalidade{
+
+    indicador: number;
+    periodo: string;
+    contexto: string;
+    localidade: number;
+
+    listaGrupos: ItemRanking[] = [];
+
+    constructor(indicador: number, periodo: string, contexto: string, localidade: number, listaGrupos: ItemRanking[]){
+        
+        this.indicador = indicador;
+        this.periodo = periodo;
+        this.contexto = contexto;
+        this.localidade = localidade;
+        this.listaGrupos = listaGrupos;
+    }
+    
+}
+
+export class ItemRanking{
+
+    constructor(localidade: Localidade, posicao: number, valor: string, fatorMultiplicativo: number = 1, unidadeMedida?: string){
+
+        this.localidade = localidade;
+        this.posicao = posicao;
+        this.valor = valor;
+        this.fatorMultiplicativo = fatorMultiplicativo;
+        this.unidadeMedida = unidadeMedida;
+    }
+
+    localidade: Localidade
+    posicao: number;
+    valor: string;
+    fatorMultiplicativo: number;
+    unidadeMedida: string;
+}
