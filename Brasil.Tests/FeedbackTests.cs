3a62f570fbe1009a61d2b688285067c0b25e6438
arthur.garcia <arthur.garcia@ibge.gov.br>
2017-07-21 13:10:08 -0300

    Funcionalidade de feedback

---
+++ Brasil.Tests/FeedbackTests.cs
@@ -0,0 +1,37 @@
+﻿using Brasil.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Brasil.Tests
+{
+    public class Class1
+    {
+        [Fact]
+        public void FeedbackValidoGeraOk()
+        {
+            var feedback = new Feedback()
+            {
+                Assunto = "Bug",
+                Email = "arthur.garcia@ibge.gov.br",
+                Mensagem = "Algo muito estranho"
+            };
+
+            var controller = new FeedbackController();
+
+            var response = controller.Index(feedback);
+        }
+
+        [Fact]
+        public void FeedbackInvalidoGeraBadRequest()
+        {
+            var feedback = new Feedback();
+
+            var controller = new FeedbackController();
+
+            var response = controller.Index(feedback);
+        }
+    }
+}
---
+++ codigofonte/Controllers/FeedbackController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+using Dapper;
+using MySql.Data.MySqlClient;
+
+namespace Brasil.Controllers
+{
+
+    public class Feedback
+    {
+        [Required]
+        [StringLength(100)]
+        [EmailAddress]
+        public string Email { get; set; }
+        [Required]
+        [StringLength(100)]
+        public string Assunto { get; set; }
+        [Required]
+        [StringLength(500)]
+        public string Mensagem { get; set; }
+        
+    }
+
+    public class FeedbackController : Controller
+    {
+        [HttpPost]
+        public IActionResult Index([FromBody] Feedback feedback)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest();
+            }
+
+            using (var conn = new MySqlConnection("server=srvbd;user id=cidadesAtend_w;password=Tj9*b7$r;database=cidades_atendimento"))
+            {
+                conn.Open();
+
+                using (var dbTransaction = conn.BeginTransaction())
+                {
+                    conn.Execute(@"
+                        INSERT INTO
+                            feedback(email, assunto, mensagem)
+                        VALUES(@email, @assunto, @mensagem)", new { email = feedback.Email, assunto = feedback.Assunto, mensagem = feedback.Mensagem  }, dbTransaction);
+                }
+
+                return Ok();
+            }
+        }
+    }
+}
