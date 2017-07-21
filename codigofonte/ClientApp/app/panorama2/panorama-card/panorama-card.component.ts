90ff6f184d362c93d8a14a8a0e9f3a2e8c216c60
Leandro de Araujo Loures Coelho <leandro.coelho@ibge.gov.br>
2017-07-21 14:04:23 -0300

    Ajuste no calculo do percentual do ranking do novo card no panorama.

--- codigofonte/ClientApp/app/panorama2/panorama-card/panorama-card.component.ts
+++ codigofonte/ClientApp/app/panorama2/panorama-card/panorama-card.component.ts
@@ -17,13 +17,25 @@ export class PanoramaCardComponent implements OnChanges {
     
     ngOnChanges(changes: any) {
         if(this.ranking && this.ranking.BR) {
-            this.cssRanking.BR = 'p' + Math.round( 100 - (this.ranking.BR.posicao / this.ranking.BR.itens)*100 );
+            this.cssRanking.BR = 'p' + this.calcularPercentualRanking(this.ranking.BR.posicao, this.ranking.BR.itens);
         }
         if(this.ranking && this.ranking.local) {
-            this.cssRanking.local = 'p' + Math.round( 100 - (this.ranking.local.posicao / this.ranking.local.itens)*100 );
+            this.cssRanking.local = 'p' + this.calcularPercentualRanking(this.ranking.local.posicao, this.ranking.local.itens);
         }
         if(this.ranking && this.ranking.microrregiao) {
-            this.cssRanking.microrregiao = 'p' + Math.round( 100 - (this.ranking.microrregiao.posicao / this.ranking.microrregiao.itens)*100 );
+            this.cssRanking.microrregiao = 'p' + this.calcularPercentualRanking(this.ranking.microrregiao.posicao, this.ranking.microrregiao.itens);
         }
+    }
+
+    private calcularPercentualRanking(posicao: number, numeroElementos: number): number{
+
+        const numeroClasses = 20;
+
+        let amplitudeClasse = numeroElementos / numeroClasses;
+        let classe = Math.round(posicao / amplitudeClasse);
+        let percentual = 100 - (classe * (100 / numeroClasses));
+
+
+        return percentual;
     }
 }
