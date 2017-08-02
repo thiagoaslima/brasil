054e219c6f7e88dc11bf2d4d77c64eb13c250c14
Fernando Mendonça de Almeida <fernando.almeida@ibge.gov.br>
2017-08-02 17:22:36 -0300

    Criação do componente PesquisaHome

---
+++ codigofonte/ClientApp/app/home/pesquisa-home.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'pesquisa-home',
+    templateUrl: 'pesquisa-home.component.html'
+})
+
+export class PesquisaHomeComponent implements OnInit {
+    constructor() { }
+
+    ngOnInit() { }
+}
