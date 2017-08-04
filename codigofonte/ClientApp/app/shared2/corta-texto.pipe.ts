0434a12234442e2eb40b22286ebc009a372a4423
Leandro A L Coelho <leandro.coelho@ibge.gov.br>
2017-08-04 15:36:41 -0300

    Correção do corta-texto.

--- codigofonte/ClientApp/app/shared2/corta-texto.pipe.ts
+++ codigofonte/ClientApp/app/shared2/corta-texto.pipe.ts
@@ -7,6 +7,11 @@ export class CortaTextoPipe implements PipeTransform {
 
     transform(value: any, max: number): any {
 
+        if(String(value).length < max){
+            
+            return value;
+        }
+
         return String(value).slice(0, max).trim() + '...';
     }
 }
