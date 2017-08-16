f20c511975aa3c4edba59ad7abc35ef2bc49f30f
Elias Correa Silva <elias.silva@ibge.gov.br>
2017-08-16 17:37:46 -0300

    Integração do formulario com o backend.

--- codigofonte/ClientApp/app/shared/questionario/questionario.component.ts
+++ codigofonte/ClientApp/app/shared/questionario/questionario.component.ts
@@ -1,8 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-
 import { isBrowser } from 'angular2-universal';
-
 import { RouterParamsService } from '../router-params.service';
+import { Http, Headers, RequestOptions, Response } from '@angular/http';
+
+const headers = new Headers({ 'accept': '*/*' });
+const options = new RequestOptions({ headers: headers, withCredentials: false });
 
 @Component({
     selector: 'questionario',
@@ -22,19 +24,56 @@ export class QuestionarioComponent implements OnInit {
     ultimaPergunta = 6;
 
     constructor(
-        private _routerParamsServ: RouterParamsService
+        private _routerParamsServ: RouterParamsService,
+        private _http: Http
     ) { }
 
     ngOnInit() {
         
     }
 
-    avancar(){
+    avancar(formulario){
         if(!this.aberto){
             this.aberto = true;
             this.label = "Avançar";
         }else if(this.pergunta < this.ultimaPergunta){
             this.pergunta += 1;
+        }
+        
+        //envia o formulário
+        if(this.pergunta == this.ultimaPergunta){
+            this._http.post("/questionario", {
+                respostas: [
+                    {
+                        questao: 1,
+                        value: formulario.pergunta1.value,
+                        observacao: null
+                    },
+                    {
+                        questao: 2,
+                        value: formulario.pergunta2.value,
+                        observacao: null
+                    },
+                    {
+                        questao: 3,
+                        value: formulario.pergunta3.value,
+                        observacao: null
+                    },
+                    {
+                        questao: 4,
+                        value: formulario.pergunta4.value,
+                        observacao: formulario.outros.value
+                    },
+                    {
+                        questao: 5,
+                        value: formulario.mensagem.value,
+                        observacao: null
+                    }
+                ]
+            }, options)
+            .subscribe(res => {
+                //console.log("ok", res);
+            });
         }
     }
 }
