1405e2dfd627909c67b06977e74ddd9ffd458db0
Thiago Lima <thiagoaslima@gmail.com>
2017-08-24 18:12:49 -0300

    correções de rotas do app (principalmente redirecionamentos para v4/...)

---
+++ codigofonte/ClientApp/app/panorama2/panorama-brasil/panorama-brasil.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'panorama-brasil',
+    templateUrl: 'panorama-brasil.component.html'
+})
+export class PanoramaBrasilComponent {
+    teste = 'Teste Panorama Brasil';
+}
--- codigofonte/ClientApp/app/panorama2/panorama2.module.ts
+++ codigofonte/ClientApp/app/panorama2/panorama2.module.ts
@@ -4,12 +4,13 @@ import { CommonModule } from '@angular/common';
 
 import { InfografiaModule } from '../infografia/infografia.module';
 import { Panorama2Service } from './panorama.service';
+import { PanoramaBrasilComponent } from './panorama-brasil/panorama-brasil.component';
 import { PanoramaShellComponent } from './panorama-shell/panorama-shell.component';
 import { PanoramaResumoComponent } from './panorama-resumo/panorama-resumo.component';
 import { PanoramaTemasComponent } from './panorama-temas/panorama-temas.component';
 import { PanoramaPainelComponent } from './panorama-painel/panorama-painel.component';
 import { PanoramaCardComponent } from './panorama-card/panorama-card.component';
-import { SharedModule3 } from "../shared3/shared3.module";
+import { SharedModule3 } from '../shared3/shared3.module';
 
 
 @NgModule({
@@ -21,6 +22,7 @@ import { SharedModule3 } from "../shared3/shared3.module";
     ],
     exports: [],
     declarations: [
+        PanoramaBrasilComponent,
         PanoramaShellComponent,
         PanoramaResumoComponent,
         PanoramaTemasComponent,
