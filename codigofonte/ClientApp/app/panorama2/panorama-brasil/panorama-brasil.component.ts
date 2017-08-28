a1601e13ac8eb26707d1a41918745598da14b86f
Fernando Mendonça de Almeida <fernando.almeida@ibge.gov.br>
2017-08-28 11:32:25 -0300

    Consumo dos serviços de indicadores conjunturais no panorama-brasil

--- codigofonte/ClientApp/app/panorama2/panorama-brasil/panorama-brasil.component.ts
+++ codigofonte/ClientApp/app/panorama2/panorama-brasil/panorama-brasil.component.ts
@@ -1,9 +1,30 @@
-import { Component } from '@angular/core';
+import { Observable } from 'rxjs/Rx';
+import { IndicadoresService } from './indicadores.service';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
     selector: 'panorama-brasil',
     templateUrl: 'panorama-brasil.component.html'
 })
-export class PanoramaBrasilComponent {
+export class PanoramaBrasilComponent implements OnInit {
     teste = 'Teste Panorama Brasil';
+
+    dados: any = {};
+
+    constructor(
+        private _indicadoresServ: IndicadoresService
+    ) {}
+
+    public ngOnInit(): void {
+        let indicadores = Object.keys(IndicadoresService.INDICADORES).map((indicador) => {
+            return this._indicadoresServ.getIndicadores(indicador, 12);
+        });
+
+        Observable.zip(...indicadores).subscribe(indicadores => {
+            debugger;
+            this.dados.indicadores = indicadores;
+        });
+
+
+    }
 }
