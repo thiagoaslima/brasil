12dc9803ec5be69ceef8a3218d41e49d2ac80199
Leandro A L Coelho <leandro.coelho@ibge.gov.br>
2017-08-29 10:54:57 -0300

    Alteração das notas de população estimada por demada legal.

--- codigofonte/ClientApp/api/notas-demanda-legal.ts
+++ codigofonte/ClientApp/api/notas-demanda-legal.ts
@@ -2,26 +2,26 @@ export const notasEspeciais = [
     {
         "localidade": 1503754,
         "indicador": 'População estimada',
-        "nota": "Por determinação judicial e para efeito de distribuição do Fundo de Participação dos Municípios, a população do Município de Jacareacanga-PA é de 41.487 habitantes. Processo Judicial número 798-41.2011.4.01.3902, Seção Judiciária de Itaituba-PA."
+        "nota": "População judicial do município de Jacareacanga-PA: 41.487 habitantes. Processo Judicial nº 798-41.2011.4.01.3902, Seção Judiciária de Itaituba-PA."
     },
     {
         "localidade": 1100205,
         "indicador": 'População estimada',
-        "nota": "Por determinação judicial o Município de Porto Velho teve suspensos os resultados do \"CENSO IBGE 2015\", conforme termo usado na Decisão Judicial, devendo-se utilizar como válidos, até futura decisão em contrário, a população estimada para 2014, que foi de 494.013 habitantes. Processo Judicial número 12316-40.2016.4.01.4100 - Seção Judiciária de Rondônia."
+        "nota": "População judicial do município de Porto Velho-RO: 494.013 habitantes. Processo Judicial nº 12316-40.2016.4.01.4100 - Seção Judiciária de Rondônia."
     },
     {
         "localidade": 2909208,
         "indicador": 'População estimada',
-        "nota": "Por determinação judicial o Município de Coronel João Sá - BA teve os efeitos das estimativas das populações de 2014, 2015 e 2016 suspensas, passando a vigorar, para efeito de distribuição do Fundo de Participação dos Municípios, a população estimada para o ano de 2013, que foi de 17.422 habitantes. Processo Judicial número 0002222-53.2017.4.01.3306 - Vara Única de Paulo Afonso-BA"
+        "nota": "População judicial do município Coronel João Sá-BA: 17.422 habitantes. Processo Judicial nº 0002222-53.2017.4.01.3306 - Vara Única de Paulo Afonso-BA."
     },
     {
         "localidade": 2508505,
         "indicador": 'População estimada',
-        "nota": "Por determinação judicial, os Municípios de Livramento e Taperoá, do estado da  Paraíba, tiveram seus limites territoriais alterados passando a vigorar, para efeitos de distribuição do Fundo de Participação dos Municípios, as populações de 7.248  e 15.316 habitantes, respectivamente. Processo Judicial número 0000301-96.2006.4.05.8200 - Tribunal de Justiça da Paraíba - PB."
+        "nota": "População judicial do município Livramento-PB: 7.262 habitantes. Processo Judicial nº 0000301-96.2006.4.05.8200 - Tribunal de Justiça da Paraíba - PB."
     },
     {
         "localidade": 2516508,
         "indicador": 'População estimada',
-        "nota": "Por determinação judicial, os Municípios de Livramento e Taperoá, do estado da  Paraíba, tiveram seus limites territoriais alterados passando a vigorar, para efeitos de distribuição do Fundo de Participação dos Municípios, as populações de 7.248  e 15.316 habitantes, respectivamente. Processo Judicial número 0000301-96.2006.4.05.8200 - Tribunal de Justiça da Paraíba - PB."
+        "nota": "População judicial do município de Taperoá-PB: 15.400 habitantes. Processo Judicial nº 0000301-96.2006.4.05.8200 - Tribunal de Justiça da Paraíba - PB."
     }
 ]
