fb610b787df83d97687c6f6b70bf45a41c56485b
Rafael Rocha <rafael.rocha@ibge.gov.br>
2017-10-06 11:24:14 -0300

    codificação componente modalErro

---
+++ codigofonte/ClientApp/app/core/modal-erro/modal-erro.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+
+@Component({
+    selector: 'modal-erro',
+    templateUrl: './modal-erro.component.html',
+    styleUrls: ['./modal-erro.component.css']
+})
+export class ModalErroComponent {
+
+}
