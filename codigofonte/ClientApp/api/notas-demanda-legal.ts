207bf0e452cdbefe5e37f39fabb79dadc10e9c90
Leandro A L Coelho <leandro.coelho@ibge.gov.br>
2017-10-30 15:12:20 -0200

    Ajuste nas nota de demanda legal.

--- codigofonte/ClientApp/api/notas-demanda-legal.ts
+++ codigofonte/ClientApp/api/notas-demanda-legal.ts
@@ -2,31 +2,31 @@ export const notasEspeciais = [
     {
         "localidade": 1503754,
         "indicador": 'População estimada',
-        "nota": "População judicial do município de Jacareacanga-PA: 41.487 habitantes. Processo Judicial nº 798-41.2011.4.01.3902, Seção Judiciária de Itaituba-PA."
+        "nota": "População judicial do município de Jacareacanga - PA: 41.487 habitantes. Processo Judicial nº 798-41.2011.4.01.3902, Seção Judiciária de Itaituba - PA."
     },
     {
         "localidade": 1100205,
         "indicador": 'População estimada',
-        "nota": "População judicial do município de Porto Velho-RO: 494.013 habitantes. Processo Judicial nº 12316-40.2016.4.01.4100 - Seção Judiciária de Rondônia."
+        "nota": "População judicial do município de Porto Velho - RO: 494.013 habitantes. Processo Judicial nº 12316-40.2016.4.01.4100 - Seção Judiciária de Rondônia."
     },
     {
         "localidade": 2909208,
         "indicador": 'População estimada',
-        "nota": "População judicial do município Coronel João Sá-BA: 17.422 habitantes. Processo Judicial nº 0002222-53.2017.4.01.3306 - Vara Única de Paulo Afonso-BA."
+        "nota": "População judicial do município Coronel João Sá - BA: 17.422 habitantes. Processo Judicial nº 0002222-53.2017.4.01.3306 - Vara Única de Paulo Afonso - BA."
     },
     {
         "localidade": 2508505,
         "indicador": 'População estimada',
-        "nota": "População judicial do município Livramento-PB: 7.262 habitantes. Processo Judicial nº 0000301-96.2006.4.05.8200 - Tribunal de Justiça da Paraíba - PB."
+        "nota": "População judicial do município Livramento - PB: 7.262 habitantes. Processo Judicial nº 0000301-96.2006.4.05.8200 - Tribunal de Justiça da Paraíba - PB."
     },
     {
         "localidade": 2516508,
         "indicador": 'População estimada',
-        "nota": "População judicial do município de Taperoá-PB: 15.400 habitantes. Processo Judicial nº 0000301-96.2006.4.05.8200 - Tribunal de Justiça da Paraíba - PB."
+        "nota": "População judicial do município de Taperoá - PB: 15.400 habitantes. Processo Judicial nº 0000301-96.2006.4.05.8200 - Tribunal de Justiça da Paraíba - PB."
     },
     {
         "localidade": 2107506,
         "indicador": 'População estimada',
-        "nota": "População judicial do município de Paço do Lumiar - MA: superior a 156.216 habitantes. Processo Judicial nº13916-98.2017.4.01.3700 - Seção Judiciária do Maranhão- MA."
+        "nota": "População judicial do município de Paço do Lumiar - MA: superior a 156.216 habitantes. Processo Judicial nº13916-98.2017.4.01.3700 - Seção Judiciária do Maranhão - MA."
     }
 ]
